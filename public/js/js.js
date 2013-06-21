e0b3eb905a69b251810361c7737e16349dfa43b5
Pinkerton <stephen@spinkerton.org>
2013-06-20 21:25:39 -0700

    Add datepicker functionality

--- public/js/js.js
+++ public/js/js.js
@@ -1,1 +1,23 @@
-$('.typeahead').typeahead();
+$(function(){
+    $('.typeahead').typeahead();
+    $('#due-date-picker').datepicker();
+
+    $("#future-one-week").click(function(event) {
+        var nextWeek = new Date();
+        nextWeek.setDate(nextWeek.getDate() + 7);
+        var year = nextWeek.getFullYear();
+        var month = nextWeek.getMonth() + 1;
+        var day = nextWeek.getDate();
+
+        if (month.toString().length < 2) {
+            month = "0" + month.toString();
+        }
+        if (day.toString().length < 2) {
+            day = "0" + month.toString();
+        }
+
+        $("#due-date-picker").val(year + "-" + month + "-" + day);
+    });
+});
+
+
