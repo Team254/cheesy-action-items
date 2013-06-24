c50c0ac8688d5459598072c747f83d522bd98246
Patrick Fairbank <patfair@gmail.com>
2013-06-23 22:52:29 -0700

    Removed foreign key constraints.

--- db/migrations/003_create_link_table.rb
+++ db/migrations/003_create_link_table.rb
@@ -2,8 +2,8 @@ Sequel.migration do
     change do
         create_table(:action_items_users) do
             primary_key :id
-            foreign_key :action_item_id, :action_items, :null => false
-            foreign_key :user_id, :users, :null => false
+            Integer :action_item_id, :null => false
+            Integer :user_id, :null => false
         end
     end
 end
