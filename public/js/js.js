ebb476f9c047e5ab4e18b71bfe3b47036d8eb257
Patrick Fairbank <patfair@gmail.com>
2013-07-11 00:29:05 -0700

    Added basic UI for adding/removing leaders.

--- public/js/js.js
+++ public/js/js.js
@@ -1,23 +1,67 @@
-$(function(){
-    $('.typeahead').typeahead();
-    $('#due_date').datepicker();
+function getCurrentLeaders() {
+  var currentLeaders = $("#leader-ids").val();
+  if (currentLeaders) {
+    return currentLeaders.split(",");
+  } else {
+    return [];
+  }
+}
 
-    $("#future-one-week").click(function(event) {
-        var nextWeek = new Date();
-        nextWeek.setDate(nextWeek.getDate() + 7);
-        var year = nextWeek.getFullYear();
-        var month = nextWeek.getMonth() + 1;
-        var day = nextWeek.getDate();
+function updateLeaderList(currentLeaders) {
+  leaderHtml = "";
+  for (var i = 0; i < currentLeaders.length; i++) {
+    leaderHtml += "<div class='alert alert-info'><a class='close' onclick='removeLeader(" + i + ")'>×</a>" +
+        leaderNames[currentLeaders[i]] + "</div>";
+  }
+  $("#leaders").html(leaderHtml);
+}
 
-        if (month.toString().length < 2) {
-            month = "0" + month.toString();
-        }
-        if (day.toString().length < 2) {
-            day = "0" + month.toString();
-        }
+function removeLeader(index) {
+  var currentLeaders = getCurrentLeaders();
+  currentLeaders.splice(index, 1);
+  $("#leader-ids").val(currentLeaders.join());
+  updateLeaderList(currentLeaders);
+}
 
-        $("#due_date").val(year + "-" + month + "-" + day);
-    });
+$(function() {
+  $("#leader-list").typeahead({
+    source: leaders,
+    updater: function(item) {
+      var currentLeaders = getCurrentLeaders();
+      currentLeaders.push(leaderIds[item]);
+      $("#leader-ids").val(currentLeaders.join());
+      updateLeaderList(currentLeaders);
+      return "";
+    }
+  });
+  $("#leader-list").keypress(function(e) {
+    // Disable the enter key from doing anything when no leader is selected.
+    if (e.which == 13) {
+      return false;
+    }
+  });
+
+  var currentLeaders = getCurrentLeaders();
+  if (currentLeaders) {
+    updateLeaderList(currentLeaders);
+  }
+
+  $("#due_date").datepicker();
+
+  $("#future-one-week").click(function(event) {
+    var nextWeek = new Date();
+    nextWeek.setDate(nextWeek.getDate() + 7);
+    var year = nextWeek.getFullYear();
+    var month = nextWeek.getMonth() + 1;
+    var day = nextWeek.getDate();
+
+    if (month.toString().length < 2) {
+      month = "0" + month.toString();
+    }
+    if (day.toString().length < 2) {
+      day = "0" + month.toString();
+    }
+
+    $("#due_date").val(year + "-" + month + "-" + day);
+  });
 });
-
-
