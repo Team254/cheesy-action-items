d1c6b96e8c7b3bb62c9b389b24445f957644611a
Patrick Fairbank <patfair@gmail.com>
2013-08-10 19:23:40 -0700

    Added preview of grade for open action items using the current date as the completion date.

--- models/action_item.rb
+++ models/action_item.rb
@@ -4,17 +4,20 @@ class ActionItem < Sequel::Model
   many_to_one :created_by_user, :class => :User, :key => :created_by_user_id
 
   def before_save
-    unless completion_date.nil?
-      days_until_due = ((due_date - completion_date) / 86400).to_i
-      if days_until_due >= 4
-        self.grade = 1.1
-      elsif days_until_due < 4 && days_until_due >= 0
-        self.grade = 0.025 * days_until_due + 1
-      else
-        self.grade = 0.5 ** (-days_until_due.to_f / 7)
-      end
-    end
+    self.grade = current_grade(completion_date) unless completion_date.nil?
     super
+  end
+
+  def current_grade(date_to = Time.now)
+    days_until_due = ((due_date - date_to) / 86400).to_i
+    if days_until_due >= 4
+      grade = 1.1
+    elsif days_until_due < 4 && days_until_due >= 0
+      grade = 0.025 * days_until_due + 1
+    else
+      grade = 0.5 ** (-days_until_due.to_f / 7)
+    end
+    grade
   end
 
   def to_json
